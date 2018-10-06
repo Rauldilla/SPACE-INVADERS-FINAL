03c518fead2394e9d1693263af11510125b268c6
David Cuenca <davidcuencaortega@gmail.com>
2018-10-06 17:35:31 +0200

    Ajuste Layout menu

--- SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/MenuActivity.java
+++ SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/MenuActivity.java
@@ -13,16 +13,13 @@ public class MenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-        final TextView endingLabel = this.findViewById(R.id.ending);
         final ImageView imagenFinal = this.findViewById(R.id.imagen);
 
         Bundle extras = getIntent().getExtras();
         final boolean win = extras.getBoolean(getResources().getString(R.string.victory));
         if (win) {
-            endingLabel.setText(R.string.victory);
             imagenFinal.setImageResource(R.drawable.win);
         } else {
-            endingLabel.setText(R.string.fail);
             imagenFinal.setImageResource(R.drawable.game_over);
         }
 
