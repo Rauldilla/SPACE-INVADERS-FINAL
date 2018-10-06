9b6d0070e3b2a582ef13d7e16c8d71abff5d7930
David Cuenca <davidcuencaortega@gmail.com>
2018-10-06 13:42:19 +0200

    Cambio de imagen

--- SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/MenuActivity.java
+++ SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/MenuActivity.java
@@ -4,21 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MenuActivity extends Activity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
         final TextView endingLabel = this.findViewById(R.id.ending);
+        final ImageView imagenFinal = this.findViewById(R.id.imagen);
 
         Bundle extras = getIntent().getExtras();
         final boolean win = extras.getBoolean(getResources().getString(R.string.victory));
         if (win) {
             endingLabel.setText(R.string.victory);
+            imagenFinal.setImageResource(R.drawable.win);
         } else {
             endingLabel.setText(R.string.fail);
+            imagenFinal.setImageResource(R.drawable.game_over);
         }
 
         final TextView scoreLabel = this.findViewById(R.id.score);
